f54d596182fc0472d209bb6986802038912a3d6c
Thiago <thiago.barboza.p@gmail.com>
2021-04-22 13:17:53 -0300

    Adding control variables to backend

--- backend/src/controllers/users.controller.js
+++ backend/src/controllers/users.controller.js
@@ -1,6 +1,5 @@
 const UsersModel = require ('../models/users.model')
 var firstBy = require('thenby');
-var thenBy = require('thenby')
 
 class UsersController{
     async index(req,res) {
@@ -10,37 +9,39 @@ class UsersController{
             firstBy('bookday')
             .thenBy('hour')
         )
-
         res.send(sorted) // Organiza os dados por dia/hora para melhor apresentação no frontend
     }
     async store (req,res){
         const data = req.body
-        const countDia = await UsersModel.countDocuments({bookday:data.bookday}) // Quantas vezes o dia da requisição aparece
-        const countHour = await UsersModel.countDocuments({bookday: data.bookday, hour:data.hour}) // Quantas vezes a hora da requisição, baseada no dia, aparece
+        const countDia = await UsersModel.countDocuments({bookday:data.bookday}); // Quantas vezes o dia da requisição aparece
+        const countHour = await UsersModel.countDocuments({bookday: data.bookday, hour:data.hour}); // Quantas vezes a hora da requisição, baseada no dia, aparece
         const isOld = 60 // Mudar essa variável caso queira que seja considerado idoso em uma idade diferente
+        const schedulePerDay = 3;// Variável que representa o número máximo de agendamentos em um dia específico
+        const schedulePerHour = 2;// Variável que representa o número máximo de agendamentos em uma hora específica
 
         // Calcula a data do limite estipulado para ser considerado idoso
-        const sixtyDate = new Date()
-        sixtyDate.setFullYear(sixtyDate.getFullYear() - isOld) 
+        const sixtyDate = new Date();
+        sixtyDate.setFullYear(sixtyDate.getFullYear() - isOld) ;
+
 
         // Corrige o formato da data escolhida para o aniversário(antes yyyy-dd-MM, agora yyyy-MM-dd) para fins de calculos
         data.birthday = data.birthday.split("/");
         var birthdayFx = new Date(data.birthday[2], parseInt(data.birthday[1], 10)-1, data.birthday[0]); 
         
-        if(countDia < 20){ // Verifica se existem menos de 20 agendamentos no dia
-            if(countHour < 2){ // Verifica se existem menos de 2 agendamentos na hora estipulada
+        if(countDia < schedulePerDay){ // Verifica se existem menos de 20 agendamentos no dia
+            if(countHour < schedulePerHour){ // Verifica se existem menos de 2 agendamentos na hora estipulada
                 const newUsers = await UsersModel.create({ // Caso sim, cria um novo agendamento
                     bookday: data.bookday,
                     hour: data.hour,
                     name: data.name,
                     birthday: birthdayFx
-                })
-                res.send({newUsers})
+                });
+                res.send({newUsers});
             }else{ // Caso hajam 2 agendamentos no dia, passará por mais verificações
                 // Verifica, nos agendamentos realizados, se existem jovens, e quantos são.
-                var isYoung = await UsersModel.find({bookday:data.bookday, hour:data.hour, birthday: {$gt: sixtyDate} })
+                var isYoung = await UsersModel.find({bookday:data.bookday, hour:data.hour, birthday: {$gt: sixtyDate} });
 
-                if(isYoung.length > 1 && sixtyDate > birthdayFx){ 
+                if(isYoung.length === 2 && sixtyDate > birthdayFx){ 
                     if(isYoung[0].birthday > isYoung[1].birthday) { // Caso hajam 2 jovens, e o primeiro seja o mais novo, será o substituído.
                         const replaced = await UsersModel.findOneAndReplace({bookday:data.bookday, hour:data.hour, birthday: isYoung[0].birthday},
                             {
@@ -49,7 +50,7 @@ class UsersController{
                                 name: data.name,
                                 birthday:birthdayFx
                             })
-                            res.send(replaced)
+                            res.send(replaced);
                     }else{ // Caso o segundo seja o mais novo, ou o aniversário de ambos sejam iguais, o segundo será substituído.
                         const replaced2 = await UsersModel.findOneAndReplace({bookday:data.bookday, hour:data.hour, birthday: isYoung[1].birthday},
                             {
@@ -58,9 +59,9 @@ class UsersController{
                                 name: data.name,
                                 birthday:birthdayFx
                             })
-                            res.send(replaced2)
+                            res.send(replaced2);
                     }
-                }else if(isYoung.length > 0.9  && sixtyDate > birthdayFx) { // Se houver apenas 1 jovem, esse será substituído
+                }else if(isYoung.length === 1  && sixtyDate > birthdayFx) { // Se houver apenas 1 jovem, esse será substituído
                     const newBooking = await UsersModel.findOneAndReplace({bookday:data.bookday, hour:data.hour, birthday: isYoung[0].birthday},
                         {
                             bookday: data.bookday,
@@ -69,25 +70,25 @@ class UsersController{
                             birthday:birthdayFx
                         }
                         )
-                     res.send(newBooking)
+                     res.send(newBooking);
                 }else{ // Caso não hajam jovens e um idoso tente escolher a hora, a hora se encontrará lotada.
-                res.status(400).send({message: 'A hora escolhida não está mais disponível'})
+                res.status(400).send({message: 'A hora escolhida não possui mais vagas.'});
                 }
             }
         }else{
-            res.status(400).send({message:"O dia escolhido não possui mais vagas."})
+            res.status(400).send({message:"O dia escolhido não possui mais vagas."});
         }
     }
     async remove(req,res){
         const { id } = req.params;
         try{
-            const user =await UsersModel.findByIdAndDelete(id);
-            if (!user){
-                throw new Error("Booking does not exist");
+            const schedule = await UsersModel.findByIdAndDelete(id); // Procura um objeto pelo seu ID, então o remove.
+            if (!schedule){
+                throw new Error("Booking does not exist"); // Caso não ache um objeto, retorna um erro.
             }
-            res.send({message: "User booking removed!"})
+            res.send({message: "User booking removed!"});
         }catch(e){
-            res.status(400).send({ message: e.message})
+            res.status(400).send({ message: e.message});
         }
     }
     async update(req, res) {
@@ -96,8 +97,7 @@ class UsersController{
             body,
         } = req;
 
-        const user = await UsersModel.findByIdAndUpdate(id, body).lean();
-        console.log(user)
+        const user = await UsersModel.findByIdAndUpdate(id, body).lean(); // Em um método PUT, procura um agendamento pelo ID e dá update no mesmo.
 
         res.send({
         user: {
--- backend/src/index.js
+++ backend/src/index.js
@@ -4,33 +4,29 @@ const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
 
-const Routes = require ("./routes/users.router")
+const Routes = require ("./routes/users.router");
 
-//importa dotenv para utilizar variáveis de ambiente
-require ('dotenv').config()
-const {HTTP_PORT, MONGO_URL} = process.env
+// Importa dotenv para utilizar variáveis de ambiente
+require ('dotenv').config();
+const {HTTP_PORT, MONGO_URL} = process.env;
 
-// conectando-se na database do mongo
+// Conectando-se na database do MongoAtlas
 mongoose.connect(MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology:true
-})
+});
 
 const app = express();
 
 app.use(cors());
-// -------bodyParser é desnecessário, ver como retirar depois------
 app.use(bodyParser.json());
 app.use(morgan("dev"));
 
-app.get("/", (req,res) => {
-    res.json({ message: "get funcionando! "})
-});
 
 // Middleware de rotas, /api/rota
 app.use("/api", Routes);
 
-// Ouve a porta escolhida, nesse caso 3636
+// "Ouve" a porta escolhida, nesse caso 3636
 app.listen(HTTP_PORT, () => {
     console.log(`Rodando na porta ${HTTP_PORT}`)
 });
